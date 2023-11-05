f00d91748d02d726910cfffb93b671c891815c07
unclethien <dxn210021@utdallas.edu>
2023-11-05 01:53:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,3 @@
-from flask import Flask 
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
